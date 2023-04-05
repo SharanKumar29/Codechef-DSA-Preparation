c886f320b7c2953013c4177c8f83917160f7a1f6
Sharan Kumar <sharankumar2029@gmail.com>
2023-04-05 09:47:08 +0530

    Delete EXPRESSION_3.py

--- EXPRESSION_3.py
+++
@@ -1,8 +0,0 @@
-t= int(input())
-for i in range(t):
-    a=list(map(int,input().split()))
-    if (+ or -)a[0](+ or -)a[1](+ or -)a[2]==0  :
-        print("YES")
-    else:
-         print("NO")
-    
